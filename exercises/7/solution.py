e97dc55f64ddfd5c10e80cb12d159258134c30cd
Edward Barak <edward.barak@gmail.com>
2018-10-29 11:06:47 -0400

    Added exercise 7

--- exercises/7/solution.py
+++ exercises/7/solution.py
@@ -1,16 +1,20 @@
-x = 0
-n = 2
+def efficient(target_prime, verbose=False):
+    n = 3
+    i = 0
+    while True:
+        isPrime = True
+        for j in range(2,n):
+            if n % j == 0:
+                isPrime = False
+                break
+        if isPrime == True:
+            i += 1
+            l = n
+        if i == target_prime-1:
+            return(n)
+        if verbose == True and n % 1000 == 0:
+            print('Current N: %i\t# of primes found: %i\tLatest prime: %i' % (n, i,l))
+        n += 1
 
-while x != 10001:
-    isPrime = True
-    for i in range(2,n+1):
-        if n % i == 0:
-            isPrime = False
-            break
-    n += 1
-    if isPrime == True:
-        x += 1
-    if n % 100 == 0:
-        print(n, x)
-
-print(n, x)
+if __name__ == '__main__':
+    print(efficient(10001, verbose=True))
