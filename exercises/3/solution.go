f1de15f1d7b6f2bedc9cc1a43107efde1efd86ba
Edward Barak <edward_barak@uhc.com>
2020-01-13 13:18:29 -0500

    created initial go solution.

---
+++ exercises/3/solution.go
@@ -0,0 +1,10 @@
+// NOTE: This does not work if largest prime factor of n > 104729
+
+package main
+
+func main() {
+    // import prime numbers csv
+    // create function that tests n % prime == 0. If True, print prime number & send value to channel
+    // use goroutines to execute function on each prime number imported
+    // use channel to wait for all goroutines to finish, then use main to determine largest factorable prime
+}
