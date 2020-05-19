b00d335a253861e781bf9665eed7137c11948aea
Ed <ed@pop-os.localdomain>
2020-05-19 17:39:12 -0400

    solution 12

--- exercises/12/solution.py
+++ exercises/12/solution.py
@@ -1,15 +1,30 @@
-class divisibleTriangle(target):
-    def __init__(self, target):
-        self.target = target
-    def findTarget(self):
-         
+from math import sqrt
+import numpy as np
 
-# start with i=1, count divisors for i using: [sum(range(0,i+1)) % j for j in range(0,sum(range(0,i+1))]
-# if divisor count < target, lowerBound = i, i*=10, count divisors for i
-# elif divisor count >= target, upperBound = i, count divisors starting at lowerBound with step=1
-# return first number to have divisor 
+def triangle_generator():
+    n = 0
+    lastTriangle = 0
+    while True:
+        n += 1
+        lastTriangle += n
+        yield (n, lastTriangle)
 
-if __name__ == '__main__':
-    from sys import argv
-    dt = divisibleTriangle(argv[0])
-    print(dt.findTarget())
+
+def count_factors(triangle):
+    squareRoot = sqrt(triangle)
+    tests = np.arange(1, int(squareRoot + 1))
+    tests = triangle % tests
+    factorsCnt = (len(tests) - len(np.extract(tests, tests))) * 2
+    if not triangle % sqrt(triangle): factorsCnt -= 1
+    return factorsCnt
+
+def first_triangle_with_gt_n_factors(n):
+    gen = triangle_generator()
+    factors = 0
+    while factors < n:
+        temp = gen.__next__()
+        factors = count_factors(temp[1])
+        print('n: {}\ttri: {}\tfactors: {}'.format(temp[0], temp[1], factors))
+
+if __name__ == "__main__":
+    first_triangle_with_gt_n_factors(500)
